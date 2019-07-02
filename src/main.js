645a4136db090c00baef1baecccdddbcff8debb7
yjinhao <yjinhao@qq.com>
2019-07-02 14:24:07 +0800

    提交商城 egou
    Signed-off-by: yjinhao <yjinhao@qq.com>

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-
+// import api from './api'
+// Vue.prototype.$api = api
+import './libs/rem.js'
+import { Row, Col, Lazyload, Tab, Tabs, Slider, Toast, Loading, Dialog, List, Field, Popup, Swipe, SwipeItem, Button } from 'vant'
 Vue.config.productionTip = false
-
+Vue.use(Button)
+Vue.use(Toast)
+Vue.use(Loading)
+Vue.use(Dialog)
+Vue.use(List)
+Vue.use(Field).use(Slider)
+Vue.use(Swipe).use(SwipeItem)
+Vue.use(Popup)
+Vue.use(Lazyload)
+Vue.use(Tab).use(Tabs)
+Vue.use(Row).use(Col)
+Vue.prototype.$toast = Toast
 new Vue({
   router,
   store,
--- src/router.js
+++ src/router.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './page/Home.vue'
 Vue.use(Router)
-
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -14,12 +12,14 @@ export default new Router({
       component: Home
     },
     {
+      path: '/index',
+      name: 'index',
+      component: () => import('./page/index.vue')
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./page/About.vue')
     }
   ]
 })
